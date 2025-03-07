2edd4a89966bd110ea52dd209adf848ed99b22a0
chairulanam1605 <limit.eren1605@gmail.com>
2025-03-07 08:30:10 +0700

    first commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Muhammad Chairul Anam"
+
+function getMyIdentity() {
+    let localVariable = 2211102016
+
+    console.log(`Nama saya ${ globalVarible }nim ${ localVariable }`)
+}
+
+console.log(globalVarible)
+    // console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,14 @@
+var name = "Muhammad Chairul Anam"
+const nim = 2211104072
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`nama saya adalah ${ name } dan nim saya adalah ${ nim }`)
+
+console.log(`Nama saya adalah ${ name } nim saya adalah ${ nim }`)
+
+
+var name = "ayam"
+
+let fullName = "Muhammad Rizqi"
